c24ecffe1498400c5232a5892f5e40ebdaad825a
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-17 09:47:16 +0300

    new project

---
+++ src/app/goal/goal.component.spec.ts
@@ -0,0 +1,49 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoalComponent } from './goal.component';
+
+describe('GoalComponent', () => {
+  let component: GoalComponent;
+  let fixture: ComponentFixture<GoalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GoalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GoalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal'
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+    goals = [
+        new Goal(1,'Watch Finding Nemo'),
+        new Goal(2,'Buy Cookies'),
+        new Goal(3, 'Get new Phone Case'),
+        new Goal(4, 'Get Dog Food'),
+        new Goal(5, 'Solve math homework'),
+        new Goal(6, 'Plot my world domination plan'),
+    ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
